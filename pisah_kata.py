485e64719291827a0d67980a7a13f0159213493e
lufias69 <lufias4@gmail.com>
2019-03-17 22:17:28 +0700

    Update pisah_kata.py

--- pisah_kata.py
+++ pisah_kata.py
@@ -2,51 +2,22 @@ import re
 kunci_jawaban = "selamat datang pada rumah kami"
 jawaban = "sselamattdadatang di rumahhh kami"
 def pisahKata(kunci_jawaban, jawaban):
-    kunci_jawaban_split = kunci_jawaban.split()
-    jawaban_split       = jawaban.split()
-    n_jawaban = []
-    for kj in kunci_jawaban_split:
-        for key, j in enumerate(jawaban_split):
-            if re.search(kj, j):
-                start = re.search(kj, j).start()
-                end   = re.search(kj, j).end()
-                if start != 0 and end != len(j):
-                    j_ = [x for x in j]
-                    j_[start] = " "+j_[start]
-                    
-                    j_[end] = " "+j_[end-1]
-                    #print(j_)
-                    #print(end)
-                    j = "".join(j_)
-                    jawaban_split[key]=j
-                    #print(j)
-                
-                elif start != 0 and end == len(j):
-                    j_ = [x for x in j]
-                    j_[start] = " "+j_[start]
-                    
-                    #j_[end] = " "+j_[end-1]
-                    #print(j_)
-                    #print(end)
-                    j = "".join(j_)
-                    jawaban_split[key]=j
-                    #print(j)
-                    #pass
-                elif start == 0 and end != len(j):
-                    j_ = [x for x in j]
-                    j_[start] = " "+j_[start]
-                    
-                    j_[end] = " "+j_[end-1]
-                    #print(j_)
-                    #print(end)
-                    j = "".join(j_)
-                    jawaban_split[key]=j
-                    #print(j)
-                    
-                else:
-                    #print("ini else", j)
-                    pass
-            #n_jawaban.append(j)
-    return " ".join(jawaban_split)
+    d_index = []
+    b_index = []
+    
+    for i in S_kunci_jawaban:
+        index_replace = [(m.start(0)) for m in re.finditer(i,jawaban)]
+        d_index += index_replace
+    
+        index_replace = [(m.end(0)) for m in re.finditer(i,jawaban)]
+        b_index += index_replace
+    jawaban_list = [x for x in jawaban]
+    for d, b in zip(d_index, b_index):
+        #print(d,b)
+        jawaban_list[d]= " "+jawaban_list[d]
+        jawaban_list[b-1]=jawaban_list[b-1]+" "
+
+    jawaban_list = re.sub(r"\s+", " ","".join(jawaban_list).rstrip().strip().lstrip())
+    return jawaban_list
 
 pisahKata(kunci_jawaban, jawaban)
